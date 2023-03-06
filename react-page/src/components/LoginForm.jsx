c01ef83c1f85d740c9d423f4624f89d093fc7213
tonchawan <tonchawan50@gmail.com>
2023-03-06 13:01:31 +0700

    update login form

--- react-page/src/components/LoginForm.jsx
+++ react-page/src/components/LoginForm.jsx
@@ -1,6 +1,42 @@
 function Login() {
+
+    const [loginData, setLoginData] = useState({
+        username: '',
+        password: '',
+      });
+
+      const handleSubmit = e => {
+        e.preventDefault();
+        console.log(loginData);
+      };
+    
+      const handleChange = e => {
+        const { name, value } = e.target;
+        setLoginData({ ...loginData, [name]: value });
+      };
+
     return (
-        <h1>this is login</h1>
+    <div>
+        <form onSubmit={handleSubmit}>
+        <label>Username:
+            <input type="text" 
+            name="prefix" 
+            value={loginData.username} 
+            onChange={handleChange} />
+        </label>
+    <br/>
+        <label>Password:
+            <input type="text" 
+            name="firstName" 
+            value={loginData.password} 
+            onChange={handleChange} />
+        </label>
+    <br/>
+       
+    
+        <button type="submit">Register</button>
+    </form>
+</div>
       )
 }
 
